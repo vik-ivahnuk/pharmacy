edaa685ea46638f007d07898111e955912f72eda
vik-ivahnuk <vik.ivahnuk@gmail.com>
2022-11-06 19:49:16 +0200

    supplier supply impl (remove useless imports)

--- src/main/java/ua/knu/pharmacy/entity/Supplier.java
+++ src/main/java/ua/knu/pharmacy/entity/Supplier.java
@@ -2,7 +2,6 @@ package ua.knu.pharmacy.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
