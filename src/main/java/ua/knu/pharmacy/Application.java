70859f3b2eff9a7f178620c010a9daa97d39c84c
vik-ivahnuk <vik.ivahnuk@gmail.com>
2022-11-06 11:01:50 +0200

    first refactoring

--- src/main/java/ua/knu/pharmacy/Application.java
+++ src/main/java/ua/knu/pharmacy/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PharmacyApplication {
+public class Application {
   public static void main(String[] args) {
-    SpringApplication.run(PharmacyApplication.class, args);
+    SpringApplication.run(Application.class, args);
   }
 }
