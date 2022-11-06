3a1d17f22157e53993b48ec0b35692b8a2f596c6
vik-ivahnuk <vik.ivahnuk@gmail.com>
2022-11-06 23:16:00 +0200

    куафсещкув

--- src/main/java/ua/knu/pharmacy/dto/response/user/UserViewProductResponse.java
+++ src/main/java/ua/knu/pharmacy/dto/response/user/UserViewProductResponse.java
@@ -3,13 +3,11 @@ package ua.knu.pharmacy.dto.response.user;
 import lombok.Builder;
 import lombok.Value;
 
-import java.time.LocalDate;
-
 @Value
 @Builder
 public class UserViewProductResponse {
-  UserViewMedicineResponse info;
-  LocalDate manufactureDate;
-  LocalDate expirationDate;
+  Long id;
+  String name;
+  String description;
   Integer count;
 }
